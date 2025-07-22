ed4294b9e4c4c4c13bfb80ab55f11fea4618d1de
arpakundu <141435533+arpakundu@users.noreply.github.com>
2025-07-22 12:44:47 +0530

    Create Diff-way-of-com-and-state.py

---
+++ Diff-way-of-com-and-state.py
@@ -0,0 +1,27 @@
+#This is a way of single line comment
+#Hello from Python
+#Hii everyone
+
+"""This is a way of multiline comment
+Hello from Python
+Hii everyone"""
+
+'''This is a way of multiline comment
+Hello from Python
+Hii everyone'''
+
+a = 1 #Single statement
+print(a)  #Statement extended over multiple line
+b = 1+2+3+\
+4+5+6+\
+7+8
+print(b)  #Statement extended over multiple line
+c = (1+2+3+
+4+5+6+
+7+8)
+print(c)
+
+d = 4; e = 5; f = 6   #put multiple statements in asingle line
+print(d)
+print(e)
+print(f)
