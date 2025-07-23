02fd90239583b664826484d8e00b5168800f3b35
arpakundu <141435533+arpakundu@users.noreply.github.com>
2025-07-23 10:32:06 +0530

    Create Assigning value using assignment operator.py

---
+++ Assigning value using assignment operator.py
@@ -0,0 +1,15 @@
+a = "ARPA KUNDU"
+
+#Multiple assignments
+b, c, d = 10, 5.5, "IQ"
+
+#Assigning the same value to the multiple variables
+x = y = z = 70
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(x)
+print(y)
+print(z)
