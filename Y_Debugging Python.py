bc06369ea54c67e4d5cd1f912fed9b646add82d2
arpakundu <141435533+arpakundu@users.noreply.github.com>
2025-08-06 22:25:23 +0530

    Create Y_Debugging Python.py

---
+++ Y_Debugging Python.py
@@ -0,0 +1,14 @@
+import pdb 
+def seq(n):
+    for i in range(n):
+        pdb.set_trace()
+        print(i)
+    return
+seq(5)
+#c:continue
+#q:quit
+#h:help
+#list
+#p:print
+#p locals()
+#p globals()
